7f61245ffd9779862a75a221c4641637b461fa38
junseo <wawajun97@gmail.com>
2025-08-31 18:23:44 +0900

    github action test

--- src/main/java/kr/heylocal/server/controller/HealthController.java
+++ src/main/java/kr/heylocal/server/controller/HealthController.java
@@ -1,13 +1,12 @@
 package kr.heylocal.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
     @GetMapping("health")
-    public String health(@RequestParam String test, @RequestParam String test2) {
+    public String health() {
         return "OK";
     }
 }
